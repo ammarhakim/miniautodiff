92bd17725c8ee67a67fb36b52293be0b9a1c6ccb
ammarhakim <a.hakim777@gmail.com>
2019-12-08 21:35:19 -0500

    More cleaner code for fetching the real() and inf() parts of a hyperreal number. Extended HyperReal type to allow any number of inf parts, though at present only a single (first derivative) is supported. More tests for functions (sqrt added)

--- GkForwardAutoDiff.h
+++ GkForwardAutoDiff.h
@@ -1,4 +1,5 @@
-// A simple implementation of AutoDiff using operator overloading
+// A simple implementation of forward-mode AutoDiff using operator
+// overloading
 
 #pragma once
 
@@ -10,43 +11,61 @@ namespace Gkyl {
   /* Hyperreal number: real + infinitesimal */
   class HyperReal {
     public:
-      HyperReal() { r[0] = 0; r[1] = 0; }
-      HyperReal(double rel) { r[0] = rel; r[1] = 0; }
-      HyperReal(double rel, double inf) { r[0] = rel; r[1] = inf; }
-      double sel(unsigned n) const { return r[n]; }
+      // various ctors
+      HyperReal() {
+        r_p = 0;
+        inf_p[0] = 0;
+      }
+      HyperReal(double rel) {
+        r_p = rel;
+        inf_p[0] = 0;
+      }
+      HyperReal(double rel, double _inf) {
+        r_p = rel;
+        inf_p[0] = _inf;
+      }
+
+      // real part
+      double real() const { return r_p; }
+      // infinitesimal part
+      double inf(unsigned n) const { return inf_p[n]; }
+      
     private:
-      double r[2]; /* r[0]: real part. r[1]: infinitesimal part */
+      double r_p; /* real part */
+      double inf_p[1]; /* Infinitesimal parts */
   };
 
-  /* Fetch parts of number based on type: provided by specializations */
-  template <typename REAL> struct F { };
+  namespace {
+    /* Fetch real part of POD number */
+    template <typename T>
+    struct _R {
+        static T g(T r) { return r; }
+    };
+    /* Fetch infinitesimal part of POD number */
+    template <typename T>
+    struct _I {
+        static T g(unsigned i, T r) { return 0; }
+    };    
 
-  template <>
-  struct F<double> {
-      static double g(unsigned i, const double& r) {
-        if (i == 0) return r; else return 0; 
-      }
-  };
-
-  template <>
-  struct F<int> {
-      static int g(unsigned i, const int& r) {
-        if (i == 0) return r; else return 0;
-      }
-  };
-  
-  template <>
-  struct F<HyperReal> {
-      static double g(unsigned i, const HyperReal& r) { return r.sel(i); }
-  };
+    /* Fetch real part of HyperReal number */
+    template <>
+    struct _R<HyperReal> {
+        static double g(const HyperReal& r) { return r.real(); }
+    };
+    /* Fetch infinitesimal part of HyperReal number */
+    template <>
+    struct _I<HyperReal> {
+        static double g(unsigned i, const HyperReal& r) { return r.inf(i); }
+    };
+  }
 
   /* Basic arithmetic operators */
 
   // binary +
   template<typename L, typename R>
   inline HyperReal operator+(const L& lv, const R& rv) {
-    double x0 = F<L>::g(0,lv), x1 = F<L>::g(1,lv);
-    double y0 = F<R>::g(0,rv), y1 = F<R>::g(1,rv);
+    double x0 = _R<L>::g(lv), x1 = _I<L>::g(0,lv);
+    double y0 = _R<R>::g(rv), y1 = _I<R>::g(0,rv);
     
     double rel = x0+y0;
     double inf = x1+y1;
@@ -56,8 +75,8 @@ namespace Gkyl {
   // binary -  
   template<typename L, typename R>
   inline HyperReal operator-(const L& lv, const R& rv) {
-    double x0 = F<L>::g(0,lv), x1 = F<L>::g(1,lv);
-    double y0 = F<R>::g(0,rv), y1 = F<R>::g(1,rv);
+    double x0 = _R<L>::g(lv), x1 = _I<L>::g(0,lv);
+    double y0 = _R<R>::g(rv), y1 = _I<R>::g(0,rv);
     
     double rel = x0-y0;
     double inf = x1-y1;
@@ -67,8 +86,8 @@ namespace Gkyl {
   // binary *  
   template<typename L, typename R>
   inline HyperReal operator*(const L& lv, const R& rv) {
-    double x0 = F<L>::g(0,lv), x1 = F<L>::g(1,lv);
-    double y0 = F<R>::g(0,rv), y1 = F<R>::g(1,rv);
+    double x0 = _R<L>::g(lv), x1 = _I<L>::g(0,lv);
+    double y0 = _R<R>::g(rv), y1 = _I<R>::g(0,rv);
     
     double rel = x0*y0;
     double inf = x0*y1+x1*y0;
@@ -79,8 +98,8 @@ namespace Gkyl {
   // binary /
   template<typename L, typename R>
   inline HyperReal operator/(const L& lv, const R& rv) {
-    double x0 = F<L>::g(0,lv), x1 = F<L>::g(1,lv);
-    double y0 = F<R>::g(0,rv), y1 = F<R>::g(1,rv);
+    double x0 = _R<L>::g(lv), x1 = _I<L>::g(0,lv);
+    double y0 = _R<R>::g(rv), y1 = _I<R>::g(0,rv);
     
     double rel = x0/y0;
     double inf = -(x0*y1-x1*y0)/(y0*y0);
@@ -90,8 +109,8 @@ namespace Gkyl {
 
   // unary -
   template<typename T>
-  inline HyperReal operator-(const T& v) {
-    double x0 = F<T>::g(0,v), x1 = F<T>::g(1,v);
+  inline HyperReal operator-(const T& x) {
+    double x0 = _R<T>::g(x), x1 = _I<T>::g(0,x);
     return HyperReal(-x0, -x1);
   }
 
@@ -99,34 +118,44 @@ namespace Gkyl {
   template<typename T>
   inline HyperReal operator+(const T& v) {
     return v;
-  }  
+  }
 
-  /* Functions from std::math library */
-  namespace m {
+  /* Derivatives of functions from std::math library */
 
+  namespace {
     // this default private struct supplies methods for use with POD
     // types (double and float)
     template <typename T>
     struct _m {
+        static T sqrt(const T& x) { return std::sqrt(x); }        
         static T cos(const T& x) { return std::cos(x); }
         static T sin(const T& x) { return std::sin(x); }
     };
-
+    
     // specialization to HyperReal number
     template <>
     struct _m<HyperReal> {
+        
+        static HyperReal sqrt(const HyperReal& x) {
+          double x0 = _R<HyperReal>::g(x), x1 = _I<HyperReal>::g(0,x);
+          double y0 = std::sqrt(x0);
+          return HyperReal(y0, 0.5*x1/y0);
+        }
+        
         static HyperReal cos(const HyperReal& x) {
-          double x0 = F<HyperReal>::g(0,x), x1 = F<HyperReal>::g(1,x);
+          double x0 = _R<HyperReal>::g(x), x1 = _I<HyperReal>::g(0,x);
           return HyperReal(std::cos(x0), -x1*std::sin(x0));
         }
+        
         static HyperReal sin(const HyperReal& x) {
-          double x0 = F<HyperReal>::g(0,x), x1 = F<HyperReal>::g(1,x);
+          double x0 = _R<HyperReal>::g(x), x1 = _I<HyperReal>::g(0,x);
           return HyperReal(std::sin(x0), x1*std::cos(x0));
         }
-    };    
-
-    // Trig functions
-    template <typename T> inline T cos(const T& x) { return _m<T>::cos(x); }
-    template <typename T> inline T sin(const T& x) { return _m<T>::sin(x); }
+    };
   }
+
+  // Math functions
+  template <typename T> inline T sqrt(const T& x) { return _m<T>::sqrt(x); }
+  template <typename T> inline T cos(const T& x) { return _m<T>::cos(x); }
+  template <typename T> inline T sin(const T& x) { return _m<T>::sin(x); }
 }
--- Unit/test_ForwardDiff.cxx
+++ Unit/test_ForwardDiff.cxx
@@ -8,69 +8,87 @@
 // these set of test ensure that we can call the Gkyl::m math
 // functions with regular POD types (double and float)
 TEST_CASE("Tests for calling spec funcs with PODs", "[pod-func]") {
-  double x =5.0;
+  double x = 5.0;
   double z;
 
   // f(x) = x*cos(x)
-  z = x*Gkyl::m::cos(x);
+  z = x*Gkyl::cos(x);
   REQUIRE( z == Approx(5.0*std::cos(5.0)) );
 
   // f(x) = x*sin(x)
-  z = x*Gkyl::m::sin(x);
+  z = x*Gkyl::sin(x);
   REQUIRE( z == Approx(5.0*std::sin(5.0)) );
 
   // f(x) = cos(x)^2 + sin(x)^2
-  z = Gkyl::m::sin(x)*Gkyl::m::sin(x) + Gkyl::m::cos(x)*Gkyl::m::cos(x);
+  z = Gkyl::sin(x)*Gkyl::sin(x) + Gkyl::cos(x)*Gkyl::cos(x);
   REQUIRE( z == Approx(1.0) );
 
   // f(x) = cos(x*sin(x))
-  z = Gkyl::m::cos(x*Gkyl::m::sin(x));
+  z = Gkyl::cos(x*Gkyl::sin(x));
   REQUIRE( z == Approx(std::cos(5*std::sin(5))) );
+
+  // f(x) = sqrt(x)
+  z = Gkyl::sqrt(x);
+  REQUIRE( z == Approx(std::sqrt(5)) );
 }
 
 TEST_CASE("Basic first derivative tests", "[simple-first-diff]") {
   Gkyl::HyperReal x(5.0, 1.0);
   Gkyl::HyperReal z;
 
+  // f(x) = 3
+  z = 3;
+  REQUIRE( z.real() == 3 );
+  REQUIRE( z.inf(0) == 0.0 );
+
   // f(x) = -x+3
   z = -x+3;
-  REQUIRE( z.sel(0) == -5.0+3 );
-  REQUIRE( z.sel(1) == -1.0 );
+  REQUIRE( z.real() == -5.0+3 );
+  REQUIRE( z.inf(0) == -1.0 );
+
+  // f(x) = 3-x
+  z = 3-x;
+  REQUIRE( z.real() == -5.0+3 );
+  REQUIRE( z.inf(0) == -1.0 );  
 
   // f(x) = 2*x^2
   z = 2*x*x;
-  REQUIRE( z.sel(0) == 2*25.0 );
-  REQUIRE( z.sel(1) == 20.0 );
+  REQUIRE( z.real() == 2*25.0 );
+  REQUIRE( z.inf(0) == 20.0 );
 
   // f(x) = x/(1+x)
   z = x/(1+x);
-  REQUIRE( z.sel(0) == Approx(5./(1+5)) );
-  REQUIRE( z.sel(1) == Approx(1./(5*5+2*5+1)) );
+  REQUIRE( z.real() == Approx(5./(1+5)) );
+  REQUIRE( z.inf(0) == Approx(1./(5*5+2*5+1)) );
 
   // f(x) = x*cos(x)
-  z = x*Gkyl::m::cos(x);
-  REQUIRE( z.sel(0) == Approx(5.0*std::cos(5.0)) );
-  REQUIRE( z.sel(1) == Approx(std::cos(5.0)-5.0*std::sin(5.0)) );
+  z = x*Gkyl::cos(x);
+  REQUIRE( z.real() == Approx(5.0*std::cos(5.0)) );
+  REQUIRE( z.inf(0) == Approx(std::cos(5.0)-5.0*std::sin(5.0)) );
 
   // f(x) = x*sin(x)
-  z = x*Gkyl::m::sin(x);
-  REQUIRE( z.sel(0) == Approx(5.0*std::sin(5.0)) );
-  REQUIRE( z.sel(1) == Approx(std::sin(5.0)+5.0*std::cos(5.0)) );
+  z = x*Gkyl::sin(x);
+  REQUIRE( z.real() == Approx(5.0*std::sin(5.0)) );
+  REQUIRE( z.inf(0) == Approx(std::sin(5.0)+5.0*std::cos(5.0)) );
 
   // f(x) = cos(x)^2 + sin(x)^2
-  z = Gkyl::m::sin(x)*Gkyl::m::sin(x) + Gkyl::m::cos(x)*Gkyl::m::cos(x);
-  REQUIRE( z.sel(0) == Approx(1.0) );
-  REQUIRE( z.sel(1) == Approx(0.0) );
+  z = Gkyl::sin(x)*Gkyl::sin(x) + Gkyl::cos(x)*Gkyl::cos(x);
+  REQUIRE( z.real() == Approx(1.0) );
+  REQUIRE( z.inf(0) == Approx(0.0) );
 
   // f(x) = cos(x*sin(x))
-  z = Gkyl::m::cos(x*Gkyl::m::sin(x));
-  REQUIRE( z.sel(0) == Approx(std::cos(5*std::sin(5))) );
-  REQUIRE( z.sel(1) == Approx(-0.4578343032148585) );
+  z = Gkyl::cos(x*Gkyl::sin(x));
+  REQUIRE( z.real() == Approx(std::cos(5*std::sin(5))) );
+  REQUIRE( z.inf(0) == Approx(-0.4578343032148585) );
 
   // f(x) = cos(w); w = x*sin(x)
-  Gkyl::HyperReal w = x*Gkyl::m::sin(x);
-  z = Gkyl::m::cos(w);
+  Gkyl::HyperReal w = x*Gkyl::sin(x);
+  z = Gkyl::cos(w);
+  REQUIRE( z.real() == Approx(std::cos(5*std::sin(5))) );
+  REQUIRE( z.inf(0) == Approx(-0.4578343032148585) );
 
-  REQUIRE( z.sel(0) == Approx(std::cos(5*std::sin(5))) );
-  REQUIRE( z.sel(1) == Approx(-0.4578343032148585) );  
+  // f(x) = sqrt(x)
+  z = Gkyl::sqrt(x);
+  REQUIRE( z.real() == Approx(std::sqrt(5)) );
+  REQUIRE( z.inf(0) == Approx(0.223606797749979) );
 }
